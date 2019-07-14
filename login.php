455bdaa93026cf377101240748b76f7153cee0e4
Vaishnavi rao Pulluri <30566748+vaishnavipulluri@users.noreply.github.com>
2019-07-14 06:55:36 +0530

    Add files via upload

--- login.php
+++ login.php
@@ -1,30 +1,99 @@
+<!--
+//login.php
+!-->
+
 <?php
+
+include('database_connection.php');
+
 session_start();
- $x=$_POST["logid"];
- $y=$_POST["password"];
-$conn = new mysqli("localhost:", "root", "","");
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-$sql = "select * from login where logid='$x';";
-$result=$conn->query($sql);
-if($result->num_rows)
-{  
-$pass=$result->fetch_assoc();
-if($y==$pass[password])
+
+$message = '';
+
+if(isset($_SESSION['user_id']))
 {
-    $_SESSION["logid"]=$x;
-    header("location:loginsuccess.php");
+ header('location:index.php');
 }
- else 
-     {
-    echo "invalid password";
-    include 'log.html';
-}
-}
-else
+
+if(isset($_POST["login"]))
 {
-    echo  "invalid loginid";
-    include 'log.html';
+ $query = "
+   SELECT * FROM login 
+    WHERE username = :username
+ ";
+ $statement = $connect->prepare($query);
+ $statement->execute(
+    array(
+      ':username' => $_POST["username"]
+     )
+  );
+  $count = $statement->rowCount();
+  if($count > 0)
+ {
+  $result = $statement->fetchAll();
+    foreach($result as $row)
+    {
+      if(password_verify($_POST["password"], $row["password"]))
+      {
+        $_SESSION['user_id'] = $row['user_id'];
+        $_SESSION['username'] = $row['username'];
+        $sub_query = "
+        INSERT INTO login_details 
+        (user_id) 
+        VALUES ('".$row['user_id']."')
+        ";
+        $statement = $connect->prepare($sub_query);
+        $statement->execute();
+        $_SESSION['login_details_id'] = $connect->lastInsertId();
+        header("location:index.php");
+      }
+      else
+      {
+       $message = "<label>Wrong Password</label>";
+      }
+    }
+ }
+ else
+ {
+  $message = "<label>Wrong Username</labe>";
+ }
 }
+
 ?>
+
+<html>  
+    <head>  
+        <title>Chat Application using PHP Ajax Jquery</title>  
+  <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    </head>  
+    <body>  
+        <div class="container">
+   <br />
+   
+   <h3 align="center">Chat Application using PHP Ajax Jquery</a></h3><br />
+   <br />
+   <div class="panel panel-default">
+      <div class="panel-heading">Chat Application Login</div>
+    <div class="panel-body">
+     <form method="post">
+      <p class="text-danger"><?php echo $message; ?></p>
+      <div class="form-group">
+       <label>Enter Username</label>
+       <input type="text" name="username" class="form-control" required />
+      </div>
+      <div class="form-group">
+       <label>Enter Password</label>
+       <input type="password" name="password" class="form-control" required />
+      </div>
+      <div class="form-group">
+       <input type="submit" name="login" class="btn btn-info" value="Login" />
+      </div>
+     </form>
+    </div>
+   </div>
+  </div>
+    </body>  
+</html>
